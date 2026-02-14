e3d34baef3e73fe1a9c8a41e8f363add5b41dd41
unknown <pamelanmuller@gmail.com>
2026-02-14 16:13:10 -0300

    adicionando screenshot

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,14 +2,17 @@ describe('login', () => {
   beforeEach(() => {
     //arrange
     cy.visit('http://localhost:4000/')
+    cy.screenshot('apos-visitar-pagina')
   })
 
 
-  it('Login com dados válidos deve permitir entrada no sistema', () => {
+  it.only('Login com dados válidos deve permitir entrada no sistema', () => {
     //act
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
+    cy.screenshot('Apos preencher dados validos')
     cy.contains('button','Entrar').click() 
+    cy.screenshot('Após clicar no botão entrar')
 
     //assert
     cy.contains('h4','Realizar Transferência').should('be.visible')
