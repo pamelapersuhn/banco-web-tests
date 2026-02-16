e38a2b833c23ee0d969cdbc5a2ee14a8600b7e96
unknown <pamelanmuller@gmail.com>
2026-02-15 21:48:30 -0300

    Adicionei o relatorio HTML

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,10 @@ module.exports = defineConfig({
 
   e2e: {
     video: false,
+    reporter: 'cypress-mochawesome-reporter',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
 });
